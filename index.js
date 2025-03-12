ffa16d6f0677160996931c649ec43c136d2ac3ca
macharia <mrmachariaorsam@gmail.com>
2025-03-12 13:11:51 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// index.js
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+
